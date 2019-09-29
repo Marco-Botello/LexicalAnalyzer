0dba7d91cc2ad9c46a6417caf0e96ff58ea09f05
Marco Botello <marco.botello@csu.fullerton.edu>
2019-09-29 16:38:46 -0700

    added KeywordCheck()

--- LexicalAnalyzer.cpp
+++ LexicalAnalyzer.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 #include<fstream>
 #include <iomanip>
+#include<string.h>
 using namespace std;
+
+//Check if the lexicalUnit is a keyword, returns true or false
+bool keywordCheck(char lexicalUnit[]){
+  char keywords[32][10] = {"auto","break","case","char","const","continue","default",
+              "do","double","else","enum","extern","float","for","goto",
+              "if","int","long","register","return","short","signed",
+              "sizeof","static","struct","switch","typedef","union",
+              "unsigned","void","volatile","while"};
+  for(int i=0;i<32;i++){
+    if(strcmp(keywords[i],lexicalUnit)==0)
+      return true;
+  }
+  return false;
+}
+
 int main(){
+
 
   ifstream input("test.txt");
   char data;
