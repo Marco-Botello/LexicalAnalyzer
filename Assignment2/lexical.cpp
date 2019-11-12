0533d9e41c74a1fba4ae8265af2c2047115dedbd
Marco Botello <marco.botello@csu.fullerton.edu>
2019-11-11 20:05:29 -0800

    Add files via upload

--- Assignment2/lexical.cpp
+++ Assignment2/lexical.cpp
@@ -1,5 +1,5 @@
 #include "lexical.h"
-int Lexical::lineNum=1;
+int Lexical::line=1;
 Lexical::Lexical(){
     input = 'a';
 }
@@ -16,8 +16,10 @@ int Lexical::column(const char c)const{
 int Lexical::intFSM(const string s){
     int state =1;
     int arr[3][2]={0,'d',1,2,2,2};
+    //accepting states
     int acc[1]={2};
     int stringSize=s.size();
+    //get column number
     for (int i=0; i<stringSize; i++){
         int co=column(s[i]);
         state=arr[state][co];
@@ -52,7 +54,7 @@ int Lexical::identifierFSM(string s){
     int stringSize= s.size();
     for (int i=0; i<stringSize; i++) {
         int co=column(s[i]);
-        state=arr[state][col];
+        state=arr[state][co];
         if(state==0){
             return 0;
         }
@@ -61,8 +63,9 @@ for (int i=0; i<4; i++) {
     if(state==acc[i]){
         return 1;
     }
-    return 0;
     }
+    return 0;
+
 }
 
 
@@ -159,5 +162,131 @@ int Lexical::identifyLexeme(string L){
     
 }
 
+void Lexical::Lexer(ifstream& f){
+    bool flag=false;
+    string s;
+    char currChar='a';
+    char previousChar='a';
+    int state=0;
+    int identify=identifyLexeme(s);
+    
+    while (!flag){
+        if(previousChar == '\n') line++;
+        currChar=f.get();
+        previousChar=currChar;
+        //check if seperator, operator, space
+        if(this->Separator(currChar)||currChar==-1||this->Operator(currChar)||currChar==' ') {
+            flag=true;
+        }
+        if((!s.empty()&&this->Separator(currChar))||(this->Operator(currChar))){
+            f.unget();
+        }
+        else if (currChar!=' ' && !(currChar==-1)){
+            s+=currChar;
+        }
+        if(!(currChar==-1)&&s.empty()){
+            flag=false;
+        }
+        
+    }
+    if(s.empty() && currChar==-1){
+        this->setToken("EOF");
+        this->setLexeme("EOF");
+        
+        return;
+    }
+    
+    if(identify==3){
+        state=identifierFSM(s);
+        this->setLexeme(s);
+        if(state==1){
+            //if it isnt an identifier it must be an identifier
+            if(checkKeyword(s))
+                this->setToken("Keyword");
+            else
+            this->setToken("Identifier");
+        }
+        else
+            this->setToken("Unknown");
+    }
+    else if(identify==1){
+        s=currChar;
+        currChar=f.peek();
+        //check for >=, <=,:= etc
+        if((s[0]=='>' && currChar=='=')|| (s[0]=='<' && currChar=='=')||(s[0]==':'&&currChar =='=')||(s[0]=='/'&&currChar=='=')){
+            s+=currChar;
+            f.get();
+        }
+        if(s==":="||s=="/="||s=="<="||s==">="||Operator(s[0])){
+            this->setLexeme(s);
+            this->setToken("Operator");
+        }
+        else{
+            this->setLexeme(s);
+            this->setToken("Unknown");
+        }
+    }
+    else if(identify==2){
+        s=currChar;
+        currChar=f.peek();
+        if(currChar=='%' && s[0]=='%'){
+            s=currChar;
+            f.get();
+        }
+        if(!(s[0]=='%')||s=="%%"){
+            this->setToken("Separator");
+            this->setLexeme(s);
+        }
+        else{
+            this->setToken("Unknown");
+            this->setLexeme(s);
+            
+        }
+    }
+    else if (identify==4){
+        state=realFSM(s);
+        this->setLexeme(s);
+        if(state==1){
+            this->setToken("Real");
+        }
+        else{
+            this->setToken("Unknown");
+        }
+    }
+    else if(identify==5){
+        state=intFSM(s);
+        this->setLexeme(s);
+        if(state==1){
+            this->setToken("Integer");
+        }
+        else
+            this->setToken("Unknown");
+    }
+    else{
+        this->setToken("Unknown");
+        this->setLexeme(s);
+        
+    }
+}
+
+
+
+void Lexical::setToken(const string newT){
+    token = newT;
+}
+void Lexical::setLexeme(const string newL){
+    lexeme=newL;
+}
+
+string Lexical::getLexeme() const{
+    return lexeme;
+}
+string Lexical::getToken() const{
+    return token;
+}
+void Lexical::print() const
+{
+    cout << "Token: "<<left<<"\t\t"<<this->token<<"Lexeme: "<<"\t\t"<<this->lexeme<<endl;
+}
 
 
