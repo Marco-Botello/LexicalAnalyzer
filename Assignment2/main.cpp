052ab0a75f69bd1dc573b5bd14f6db11fd07798b
Marco Botello <marco.botello@csu.fullerton.edu>
2019-11-11 16:59:05 -0800

    Add files via upload

--- Assignment2/main.cpp
+++ Assignment2/main.cpp
@@ -1,1 +1,39 @@
+#include <string>
+#include<iostream>
+#include<fstream>
+#include<iomanip>
+#include<ctype.h>
+#include "lexical.h"
+#include "Par.h"
+using namespace std;
+int main(){
+    ifstream input;
+    ofstream output("output.txt");
+    string line,file;
+     int onOff=0;
+        //allow user to choose file instead of always being test.txt
+        cout<< "Enter the input filename:"<<endl;
+        cin>>file;
+        input.open(file.c_str());
+        //check if file opened
+        if(!input.is_open()){
+            cout<<"Failed to open file."<<endl;
+            return 0;
+        }
+    cout<<"This file's tokens and lexemes:"<<endl;
+    output<<"This file's tokens and lexemes:"<<endl;
+    Parser p;
+    input.clear();
+    //set position back
+    input.seekg(0);
+    cout<< "Press 1 to turn on Syntax Analysis or enter 0 to continue with just a Lexical Analysis."<<endl;
+    cin>>onOff;
 
+    //Turns on syntax check
+    p.ParserOnff(switch);
+    p.RAT19F(input,output);
+        input.close();
+    output.close();
+    return 0;
+
+}
